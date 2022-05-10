15e3067564527d08fbece72edde1747acb8654e0
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2022-05-10 12:47:05 -0300

    .

--- index.js
+++ index.js
@@ -7,201 +7,25 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
-    args: ["--start-maximized"],
+    args: ["--start-maximized", "--mute-audio"],
   });
   const page = await browser.newPage();
   await page.goto("https://www.flashscore.com.br/");
 
+  // accept cookies
   await (await page.waitForSelector("#onetrust-accept-btn-handler")).click();
 
-  function convertToTime(time = 0) {
-    let preTime = "";
-    let seconds = 0;
-    let minutes = 0;
-    let hours = 0;
+  // click on live matches
+  await page.evaluate(() => {
+    const menuText = [...document.querySelectorAll('[class="filters__text filters__text--short"]')]
+    menuText[1].click()
+  });
 
-    if (typeof time != "string") {
-      preTime = time.toString().trim();
-    } else {
-      preTime = time.trim();
-    }
-
-    if (preTime.indexOf("+") != -1) {
-      preTime = preTime.substring(0, preTime.indexOf("+"));
-    }
-
-    let arr = "";
-
-    if (preTime.indexOf(":") == -1) {
-      minutes = Number(time);
-    } else {
-      arr = time.split(":").map(Number);
-    }
-    if (arr.length == 2) {
-      minutes = arr[0];
-      seconds = arr[1];
-    } else if (arr.length == 3) {
-      hours = arr[0];
-      minutes = arr[1];
-      seconds = arr[2];
-    } else {
-      return false;
-    }
-    return hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000;
-  }
-
-  const timeActually = () => {
-    const date = new Date();
-    return date.toLocaleTimeString();
-  };
-
-  const getDailyMatches = await page.evaluate(() => 
-    newMatchesDaily = [
-      ...document.querySelectorAll('[title="Clique para detalhes do jogo!"]'),
-    ]
-      .map((match) =>
-        match.attributes[0].textContent
-          .concat("\n")
-          .concat(match.innerText)
-          .concat(
-            "\n" +
-              (match.childNodes[3].childNodes == 1
-                ? ""
-                : match.childNodes[3].childNodes.length - 1)
-          )
-          .concat(
-            "\n" +
-              (match.childNodes[5].childNodes == 1
-                ? ""
-                : match.childNodes[5].childNodes.length - 1)
-          )
-      )
-      .map((match) => match.split("\n"))
-      .map((match) => {
-        let data = {
-          id: '',
-          status: '',
-          teams: {
-            home: '',
-            way: ''
-          },
-          score: {
-            home: '',
-            way: ''
-          },
-          redCards: {
-            home: '',
-            way: ''
-          }
-        }
-        
-        switch (match.length) {
-          case 12:
-            [
-              data.id,
-              data.status,
-              data.teams.home,
-              data.teams.way,
-              data.score.home,
-              data.score.way,
-              ,
-              ,
-              ,
-              ,
-              data.redCards.home,
-              data.redCards.way
-            ] = match;
-            break;
-
-          case 11:
-            [
-              data.id,
-              data.status,
-              data.teams.home,
-              data.teams.way,
-              data.score.home,
-              data.score.way,
-              ,
-              ,
-              ,
-              data.redCards.home,
-              data.redCards.way
-            ] = match;
-            break;
-          
-          case 10:
-            [
-              data.id,
-              data.status,
-              data.teams.home,
-              data.teams.way,
-              data.score.home,
-              data.score.way,
-              ,
-              ,
-              data.redCards.home,
-              data.redCards.way
-            ] = match;
-            break;
-
-          case 9:
-            [
-              data.id,
-              data.status,
-              ,
-              data.teams.home,
-              data.teams.way,
-              data.score.home,
-              data.score.way,
-              data.redCards.home,
-              data.redCards.way
-            ] = match;
-            break;
-
-          case 8:
-            [
-              data.id,
-              data.status,
-              data.teams.home,
-              data.teams.way,
-              data.score.home,
-              data.score.way,
-              data.redCards.home,
-              data.redCards.way
-            ] = match;
-            break;
-
-          case 6:
-            [ 
-              data.id,
-              data.status,
-              data.teams.home,
-              data.teams.way
-            ] = match;
-            break;
-        }
-        
-        return [
-            data.id,
-            data.status,
-            data.teams.home,
-            data.teams.way,
-            data.score.home,
-            data.score.way,
-            data.redCards.home,
-            data.redCards.way
-        ];
-      })
-      .map((match) => match.map((text) => text.trim()))
-  );
-
-  getDailyMatches
-    .filter(match => allMatchesDaily
-      .filter(data =>
-        data[2].indexOf(match[2]) == -1)
-      .forEach(match => allMatchesDaily.push(match)));
+  // get all id's
+  const ids = await page.$$eval('[title="Clique para detalhes do jogo!"]', result => result.map(div => div.attributes[0].textContent))
   
-  allMatchesDaily.filter(match => match[1].length == 1 || match[1].length == 2).forEach(match => matchesInLive.push(match));
-  console.log(allMatchesDaily)
+  for (let i=0; i<ids.length; i++) {
+    await (await page.waitForSelector(`#${ids[i]}`)).click();
+  }
 
 })();
